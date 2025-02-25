63e15f7f2c941c84f3a5d938494c51e189d9ffb8
dataman <hieudz20041999@gmail.com>
2025-02-25 23:27:28 +0700

    update pom

--- lab1/java_rmi/server/src/main/java/org/app/OrderClient.java
+++ lab1/java_rmi/server/src/main/java/org/app/OrderClient.java
@@ -6,7 +6,7 @@ import java.rmi.registry.Registry;
 public class OrderClient {
   public static void main(String[] args) {
     try {
-      Registry registry = LocateRegistry.getRegistry("localhost", 1099);
+      Registry registry = LocateRegistry.getRegistry("192.168.33.10", 1099);
       OrderService service = (OrderService) registry.lookup("OrderService");
       String response = service.calculateTotal("00288978-506e-40e1-93c8-954390f3032c", 2);
       System.out.println(response);
